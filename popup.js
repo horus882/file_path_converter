f715b0df354ed44f1c2e5df9fcddd11b9816a704
Brent Hsieh <horus882@gmail.com>
2021-12-30 19:06:30 +0800

    feat: 新增 mac to windows 的選項

--- popup.js
+++ popup.js
@@ -4,7 +4,16 @@ const result    = document.getElementById('result');
 
 convert.addEventListener('click', () => {
 
-    let value = path.value.replace(/\\/g, '/');
-    result.textContent = value;
+    let system  = document.querySelector('input[name="system"]:checked').value;
+    let value   = path.value;
+
+    switch(system) {
+        case 'windows_to_mac':
+            result.textContent = value.replace(/\\/g, '/');
+            break;
+        case 'mac_to_windows':
+            result.textContent = value.replace(/\//g, '\\');
+            break;
+    }
 
 });
